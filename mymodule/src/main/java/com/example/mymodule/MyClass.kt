2035a93970daef03247fa219378d61d0e460d833
Carmen Alvarez <calvarez@libon.com>
2021-11-03 09:46:22 +0100

    Replace the Range lint issue with a MissingPermission issue

--- mymodule/src/main/java/com/example/mymodule/MyClass.kt
+++ mymodule/src/main/java/com/example/mymodule/MyClass.kt
@@ -1,23 +1,15 @@
 package com.example.mymodule
 
-import android.content.Context
-import android.net.Uri
+import android.Manifest
+import androidx.annotation.RequiresPermission
 
 object MyClass {
     fun foo(
-        context: Context,
-        uri: Uri,
-        projection: Array<String>,
-        selection: String,
-        selectionArgs: Array<String>,
-        sortOrder: String
     ) {
-        val cursor =
-            context.contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)
-        cursor?.use {
-            if (it.moveToNext()) {
-                val foo = it.getString(it.getColumnIndex("Foo"))
-            }
-        }
+        fooImpl()
+    }
+
+    @RequiresPermission(Manifest.permission.RECORD_AUDIO)
+    private fun fooImpl() {
     }
 }
